25d7d1e548f6028bcd9dadb23f14747c652bc6bb
Anshuman <anshumanshukla.12as@gmail.com>
2024-06-29 08:28:56 +0530

    Timeline

--- src/App.jsx
+++ src/App.jsx
@@ -44,25 +44,64 @@
 // export default App
 
 // Revision
-import React, { useRef } from 'react'
+// import React, { useRef } from 'react'
+// import {useGSAP} from '@gsap/react'
+// import gsap from 'gsap';
+// const App = () => {
+//     const boxRef = useRef();
+//     useGSAP(()=>{
+//         gsap.to(boxRef.current,{
+//             x:800,
+//             delay:1,
+//             duration:4,
+//             backgroundColor:"yellow",
+//             rotate:360,
+//         })
+//     })
+//   return (
+//     <div>
+//       <div ref={boxRef} className="ball"></div>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// ONE STEP UP
+
+import React from 'react'
 import {useGSAP} from '@gsap/react'
-import gsap from 'gsap';
+import gsap from 'gsap'
+import { useRef } from 'react'
 const App = () => {
-    const boxRef = useRef();
+    const boxRef = useRef()
+    const circleRef = useRef();
+    const tl = gsap.timeline()
     useGSAP(()=>{
-        gsap.to(boxRef.current,{
-            x:800,
-            delay:1,
-            duration:4,
-            backgroundColor:"yellow",
-            rotate:360,
-        })
+    tl.to(boxRef.current,{
+      x:1000,
+      rotate:360,
+      borderRadius:"50%",
+      duration:3,
+      delay:1,
+
+    })
+    gsap.to(circleRef.current,{
+      x:1000,
+      rotate:360,
+      borderRadius:"10px",
+      duration:3,
+     
+
+    })
     })
   return (
     <div>
-      <div ref={boxRef} className="ball"></div>
+      <div ref={boxRef} className="box"></div>
+      <div ref={circleRef} className="circle"></div>
     </div>
   )
 }
 
 export default App
+
