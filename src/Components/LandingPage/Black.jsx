74255f9c7d36b5e354301d513949928b683d4047
Anshuman <anshumanshukla.12as@gmail.com>
2024-07-01 21:42:23 +0530

    Animation

--- src/Components/LandingPage/Black.jsx
+++ src/Components/LandingPage/Black.jsx
@@ -3,24 +3,19 @@ import gsap from 'gsap';
 import React, { useRef } from 'react'
 const Black = () => {
     const blackRef = useRef(null);
-    const pageRef = useRef(null)
+   
     useGSAP(()=>{
-        gsap.to(homeRef.current,{
+        gsap.to(blackRef.current,{
             y:"-100%",
-            delay:1.8,
-            duration:0.5
+            delay:1.5,
+            duration:0.8,
+            rotate:0
         })
-    useGSAP(()=>{
-        gsap.to(pageRef.current,{
-            opacity:1,
-            delay:2.1,
-            duration:0.5
-        })
-    })
+    
     })
   return (
    <div>
-     <div ref = {blackRef} className="h-screen bg-[#111] w-full fixed z-10"></div>
+     <div ref = {blackRef} className="rotate-12 h-screen bg-[#111] w-full absolute z-20"></div>
    </div>
   )
 }
--- src/Components/LandingPage/Home.jsx
+++ src/Components/LandingPage/Home.jsx
@@ -3,15 +3,23 @@ import NavBar from '../Navbar/NavBar'
 import CenterText from '../Center/CenterText'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import Black from './Black'
 
 const Home = () => {
-    
+    const pageRef = useRef(null)
+    useGSAP(()=>{
+        gsap.to(pageRef.current,{
+            opacity:1,
+            delay:2.1,
+            duration:0.5
+        })
+    })
    
     
 
   return (
-    <div className='h-screen'>
-        
+    <div className=''>
+        <Black/>
         <div ref={pageRef}  className='opacity-0 h-screen w-full  bg-cover bg-center bg-[url(https://images.prismic.io/arock-website-2023/d4147892-cb03-4582-af32-326bb109803f_AROCK-_1502.jpg?fm=webp&q=100&fit=crop)] '>
       <NavBar/>
       <CenterText/>
