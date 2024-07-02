f8df3333cdf8eb442d2fe07775f31ceaf5d93b5f
Anshuman <anshumanshukla.12as@gmail.com>
2024-07-02 19:42:57 +0530

    Update

--- src/Components/LandingPage/Black.jsx
+++ src/Components/LandingPage/Black.jsx
@@ -1,7 +1,7 @@
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import React, { useRef } from 'react'
-import {useEffect} from 'react'
+
 const Black = () => {
     const blackRef = useRef(null);
    
