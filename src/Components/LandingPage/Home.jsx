dff502ff9faba39a884fc3773d6817941bfac693
Anshuman <anshumanshukla.12as@gmail.com>
2024-07-04 21:45:06 +0530

    New State

--- src/Components/LandingPage/Home.jsx
+++ src/Components/LandingPage/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
   
     const [data,setData] = useState(0);
   return (
-    <div className=''>
+    <div className='m-10 p-4 flex items'>
         <Black/>
         <div ref={pageRef}  className='opacity-0 h-screen w-full  bg-cover bg-center bg-[url(https://images.prismic.io/arock-website-2023/d4147892-cb03-4582-af32-326bb109803f_AROCK-_1502.jpg?fm=webp&q=100&fit=crop)] '>
       <NavBar {data}/>
